7c9176e140dd2b9664362045e3eb6f604be7e2c8
Mark A Lang <mlang12@Marks-MBP.home>
2017-04-24 17:08:23 -0400

    Added effects. Fixed bugs

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -8,7 +8,8 @@ window.onload = function(){
   var c = document.getElementById("c");
   var d = document.getElementById("d");
   var choices = document.getElementsByClassName("choice");
-  var timeDiv = document.getElementById("timerHolder");
+  var msgHolder = document.getElementById("msgHolder");
+  var board = document.getElementById("main")
 
   var i = 0;
 
@@ -24,7 +25,11 @@ window.onload = function(){
       ["Category: Countries", "What is the largest country by area?", "China", "Russia", "United States", "Canada", "b"],
       ["Category: Countries", "What country has the largest population?", "Russia", "India", "United States", "China", "d"],
       ["Category: Countries", "What is the largest island by area that is also a country?", "Australia", "Madigascar", "Indonesia", "Japan", "c"],
-      ["Category: Countries", "What is the most visited country in the world?", "England", "United States", "Japan", "France", "d"]
+      ["Category: Countries", "What is the most visited country in the world?", "England", "United States", "Japan", "France", "d"],
+      ["Category: Bodies of Water", "What is the longest river in the world?", "Amazon", "Nile", "Mississippi", "Hudson", "a"],
+      ["Category: Bodies of Water", "What is the biggest lake in the world?", "Superior", "Caspian", "Huron", "Victoria", "b"],
+      ["Category: Bodies of Water", "What is the smallest ocean?", "Arctic", "Indian", "Atlantic", "Pacific", "a"],
+      ["Category: Bodies of Water", "Which of the following is not a sea?", "Mediterranean", "Caribean", "Black", "Bangoo", "d"]
     ],
 
     //Contains the variables that refer to the areas of DOM to be
@@ -36,12 +41,11 @@ window.onload = function(){
       b: b,
       c: c,
       d: d
-      //timeDiv
     },
 
     //The current game timer - default is 25 seconds to answer a question and 5 seconds
     //in between questions
-    timer: 250,
+    timer: 100,
     displayTimer: "",
 
     //Current question number
@@ -59,7 +63,7 @@ window.onload = function(){
     message: "",
 
     //the current game mode. Used to trigger certain events
-    mode: "preGame", //modes are: preGame, inQuestion, afterQuestion, endRound
+    mode: "preGame", //modes are: preGame, inQuestion, afterQuestion, endRound, outOfQuestions
 
     //Keeps track of the right and wrong guesses
     scoreboard: {
@@ -72,83 +76,107 @@ window.onload = function(){
     updateTimer: function(){
       game.timer --
       game.displayTimer = String(Math.ceil(game.timer/10));
-      if (game.timer < 90 && game.timer >= 0 ){
+      if (game.timer <= 90 && game.timer >= 0 ){
         game.displayTimer = "0" + Math.ceil(game.timer/10)
       } else if (game.timer< 0) {
         game.displayTimer = "00" 
       }
-      console.log(game.mode)
-      game.onTick(game.mode)
+      game.onTick(game.mode); //Updates game based on the game mode
     },
 
     //This function does checks on every 'tick' of the game time to watch for game state changes
     //and make calls and updates variables as appropriate.
     onTick: function(mode){
+            //if there are no more questions show the end of questions screen and allow user to reset game
+      if(this.pastQuestions.length >= this.question.length){
+        this.message = "Total Correct: " + this.scoreboard.totalWins + 
+          "<br> Total Wrong: " + this.scoreboard.totalLosses 
+        this.mode = "outOfQuestions"
+        this.display.category.innerHTML = "Push the button to reset the game.";
+        this.display.question.innerHTML =  "Oops we ran out of questions!";
+        this.display.a.innerHTML = "Reset";
+        this.displayMode()
+        return;
+      }
+
+      //When the game is in preGame mode (on load)
       if(mode === "preGame"){
         this.display.category.innerHTML = "Push the button to get started.";
         this.display.question.innerHTML =  "Welcome to GeoTrivia";
         this.display.a.innerHTML = "Let's Play!";
-        this.display.b.style.display = "none";
-        this.display.c.style.display = "none";
-        this.display.d.style.display = "none";
+        this.displayMode()
+        return
       }
       
+      //When the user is currently "in a question" and computer awaiting input
       if(mode === "inQuestion"){
         var _this = this
         i = 0
-        timeDiv.style.visibility = "visible";
         
+        //Update the gameboard with items of the current question
         Object.keys(this.display).forEach(function(el){
           _this.display[el].innerHTML = _this.question[_this.currentQuestion][i];
-          _this.display[el].style.display = "block";
           if(el.length === 1 ){
-            _this.display[el].className = "choice";
+            _this.display[el].className = "choice"; //resets the class names to clear formats from correct/incorrect answers
           }
           i++;
         });
-        
-        timeDiv.innerHTML = this.displayTimer;
-      
+        board.className = "row boardBg";
+        this.displayMode()
 
+        //When the timer hits zero and user hasn't made a guess
         if(this.timer <= 0 ){
           this.mode = "afterQuestion";
           this.scoreboard.losses++;
-          this.message = "Out of time!";
+          this.message = "Out of time! <br> The right answer was \"" 
+            + this.question[this.currentQuestion][this.question[this.currentQuestion].length-1].toUpperCase()
+            + "\""
           this.timer = 30;
           this.remainingQuestions--;
+          this.display[this.question[this.currentQuestion][this.question[this.currentQuestion].length-1]].className = "choice missedGuess";
+          board.className = "row boardWrong"
         }
 
-        timeDiv.innerHTML = this.displayTimer;
+        msgHolder.innerHTML = this.displayTimer; //Update the timer
+        return;
       }
       
+      //If the game is "after a question" in the brief period in between questions
       if(mode === "afterQuestion"){
-        timeDiv.innerHTML = this.message
+        msgHolder.innerHTML = this.message
         
-        if (this.timer <= 0) {
+        if (this.timer <= 0 && this.remainingQuestions >= 1) {
           this.mode = "inQuestion";
-          this.timer = 250;
+          this.timer = 100;
+          this.pastQuestions.push(this.currentQuestion);
           this.currentQuestion = this.getNewQuestion();
         }
 
-        if (this.remainingQuestions === 0){
+        if (this.timer <= 0 && this.remainingQuestions <= 0) {
+          this.pastQuestions.push(this.currentQuestion);
+          this.currentQuestion = this.getNewQuestion();          
           this.mode = "endRound";
           this.remainingQuestions = this.questionsPerRound;
           this.scoreboard.totalWins += this.scoreboard.wins;
           this.scoreboard.totalLosses += this.scoreboard.losses;
         }
+        return;
       }
       
+      //If the current round is over
       if(mode === "endRound"){
-        this.message = "Round Complete. <br> " + this.scoreboard.totalWins + " total correct. <br>" + this.scoreboard.totalLosses + " total wrong."
+        board.className = "row boardBg";
+        this.message = this.scoreboard.totalWins + 
+          " total correct. <br>" + this.scoreboard.totalLosses + " total wrong."
         this.display.category.innerHTML = "Round Complete";
-        this.display.question.innerHTML =  "You got " + this.scoreboard.wins + " correct <br> and " + this.scoreboard.losses + " wrong.";
+        this.display.question.innerHTML =  "You got " + this.scoreboard.wins + 
+          " correct <br> and " + this.scoreboard.losses + " wrong.";
         this.display.a.innerHTML = "Next Round";
         this.display.a.className = "choice";
-        this.display.b.style.display = "none";
-        this.display.c.style.display = "none";
-        this.display.d.style.display = "none";
-        timeDiv.innerHTML = this.message;
+        this.displayMode();
+        return;
       }
+  
     },
 
     init: function(answerButtons) {
@@ -166,7 +194,7 @@ window.onload = function(){
     select: function(el){
       if (game.mode === "preGame"){
         game.mode = "inQuestion";
-        this.timer = 250;
+        this.timer = 100;
         return;
       }
 
@@ -176,11 +204,15 @@ window.onload = function(){
           this.scoreboard.wins ++;
           this.display[el].className = "choice correct";
           this.message = "Correct!";
+          board.className = "row boardCorrect"
         } else { //If the user didn't make the correct selection
           this.scoreboard.losses ++;
           this.display[el].className = "choice incorrect";
-          this.display[this.question[this.currentQuestion][this.question[this.currentQuestion].length-1]].className = "choice correct";
-          this.message = "Sorry, not correct...";
+          board.className = "row boardWrong"
+          this.display[this.question[this.currentQuestion][this.question[this.currentQuestion].length-1]].className = "choice missedGuess";
+          this.message = "Sorry, the right answer was \"" 
+            + this.question[this.currentQuestion][this.question[this.currentQuestion].length-1].toUpperCase()
+            + "\"";
         }
 
         //Change the game mode, reset timer to 'in-between questions time limit' reduce the amount of questions in round
@@ -190,13 +222,25 @@ window.onload = function(){
         return;
       }
 
+      //If the round is over then there is only 1 button to click which will move into next round
       if (game.mode === "endRound"){
         game.mode = "inQuestion";
+        this.timer = 100;
         this.scoreboard.wins = 0;
         this.scoreboard.losses = 0;
         return;
       }
 
+      //If the game is out of questions, the button will reset the game
+      if(this.mode === "outOfQuestions"){
+        this.mode = "preGame"
+        this.scoreboard.wins = 0;
+        this.scoreboard.losses = 0;
+        this.scoreboard.totalWins = 0;
+        this.scoreboard.totalLosses = 0;
+        this.pastQuestions = [];
+        this.message = ""
+      }
     },
 
     //Returns a new quesiton from the library that wasn't already played
@@ -206,9 +250,26 @@ window.onload = function(){
         tempnum = Math.floor(Math.random() * this.question.length);
       }
       return tempnum;
+    },
+
+    //This function is called during certain redraws (usually from onTick) and sets certain elements
+    //to display or not and updates the msgHolder element
+    displayMode: function(){
+      if(this.mode === "inQuestion" || this.mode === "afterQuestion"){
+        this.display.b.style.display = "block";
+        this.display.c.style.display = "block";
+        this.display.d.style.display = "block";
+
+      }
+      
+      if(this.mode === "preGame" || this.mode === "outOfQuestions" || this.mode === "endRound"){
+        this.display.b.style.display = "none";
+        this.display.c.style.display = "none";
+        this.display.d.style.display = "none";
+      }
+
+      msgHolder.innerHTML = this.message
     }
-
-
   } //Close Game Obj
   var cancelInterval = setInterval(game.updateTimer, 100);
   game.init(choices);
